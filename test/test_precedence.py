b272128a717939af9a46a4d7bacc0f96bdba0b2e
Michael Driscoll <mbdriscoll@gmail.com>
2014-02-20 12:07:29 -0800

    add tests for paren handling within trees of operators with the same precedence

--- test/test_precedence.py
+++ test/test_precedence.py
@@ -70,3 +70,58 @@ class TestPrecedence(unittest.TestCase):
     a, b, c = self.args
     tree = PostInc(Sub(a))
     self._check(tree, "(-a)++")
+
+
+class TestAssociativityPrecedence(unittest.TestCase):
+  """
+  Tests insertion of parentheses for trees of operators that
+  have the same precedence, and therefore we need to consider
+  their associativity.
+  """
+
+  def setUp(self):
+    self.args = (SymbolRef('a'), SymbolRef('b'), SymbolRef('c'))
+
+  def _check(self, tree, expected):
+    actual = str(tree)
+    self.assertEqual(actual, expected)
+
+  def test_subadd_rhs(self):
+    a, b, c = self.args
+    tree = Sub(a, Add(b, c))
+    self._check(tree, "a - (b + c)")
+
+  def test_subadd_lhs(self):
+    a, b, c = self.args
+    tree = Sub(Add(a, b), c)
+    self._check(tree, "a + b - c")
+
+  def test_complicated_00(self):
+    a, b, c = self.args
+    tree = Sub(Add(a, Sub(b, a)), Add(Sub(a, b), c))
+    self._check(tree, "a + (b - a) - (a - b + c)")
+
+  def test_augassign(self):
+    a, b, c = self.args
+    tree = AddAssign(a, AddAssign(b, c))
+    self._check(tree, "a += b += c")
+
+  @unittest.skip("Pending paren handling for ternary ops")
+  def test_ternary_cond(self):
+    a, b, c = self.args
+    cond = TernaryOp(a, b, c)
+    tree = TernaryOp(cond, c, b)
+    self._check(tree, "(a ? b : c) ? c : b")
+
+  @unittest.skip("Pending paren handling for ternary ops")
+  def test_ternary_then(self):
+    a, b, c = self.args
+    then = TernaryOp(a, b, c)
+    tree = TernaryOp(c, then, a)
+    self._check(tree, "c ? (a ? b : c) : b")
+
+  def test_ternary_else(self):
+    a, b, c = self.args
+    elze = TernaryOp(a, b, c)
+    tree = TernaryOp(c, b, elze)
+    self._check(tree, "c ? b : a ? b : c")
