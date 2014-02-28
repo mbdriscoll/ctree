739e9b8ebf5bdfe31a7087b6cce7a2b2399fd91a
Michael Driscoll <mbdriscoll@gmail.com>
2014-02-27 23:09:25 -0800

    compare fixture functions against AST versions

--- test/test_jit.py
+++ test/test_jit.py
@@ -9,20 +9,20 @@ class TestJit(unittest.TestCase):
     mod = JitModule()
     mod.load(identity_ast)
     c_identity_fn = mod.get_callable(identity_ast)
-    self.assertEqual(1, c_identity_fn(1))
-    self.assertEqual(12, c_identity_fn(12))
-    self.assertEqual(123, c_identity_fn(123))
+    self.assertEqual(identity(1), c_identity_fn(1))
+    self.assertEqual(identity(12), c_identity_fn(12))
+    self.assertEqual(identity(123), c_identity_fn(123))
 
   def test_fib(self):
     mod = JitModule()
     mod.load(fib_ast)
     c_fib_fn = mod.get_callable(fib_ast)
-    self.assertEqual(1, c_fib_fn(1))
-    self.assertEqual(8, c_fib_fn(6))
+    self.assertEqual(fib(1), c_fib_fn(1))
+    self.assertEqual(fib(6), c_fib_fn(6))
 
   def test_gcd(self):
     mod = JitModule()
     mod.load(gcd_ast)
     c_gcd_fn = mod.get_callable(gcd_ast)
-    self.assertEqual(2, c_gcd_fn(44, 122))
-    self.assertEqual(3, c_gcd_fn(27, 39))
+    self.assertEqual(gcd(44, 122), c_gcd_fn(44, 122))
+    self.assertEqual(gcd(27, 39), c_gcd_fn(27, 39))
