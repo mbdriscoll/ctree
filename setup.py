14bec5e4ef0f2b90090fd1f634f25c698effcc05
Michael Driscoll <mbdriscoll@gmail.com>
2014-03-02 11:42:03 -0800

    run setup.py install

--- setup.py
+++ setup.py
@@ -2,6 +2,18 @@ from distutils.core import setup
 
 setup(
   name='ctree',
-  version='0.95',
-  packages=['ctree'],
+  version='0.95a',
+  packages=['ctree',
+    'ctree.c',
+    'ctree.cilk',
+    'ctree.cpp',
+    'ctree.ocl',
+    'ctree.omp',
+    'ctree.py',
+    'ctree.sse',
+  ],
+
+  package_data = {
+    'ctree': ['defaults.cfg'],
+  },
 )
