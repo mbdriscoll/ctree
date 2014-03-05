3892ca574e1220ec55bba5b48f26852cbbb3d58f
chick <chick@eecs.berkeley.edu>
2014-03-05 08:40:02 -0800

    reformat examples package

--- examples/ArrayDoubler.py
+++ examples/ArrayDoubler.py
@@ -3,6 +3,7 @@ Parses the python AST below, transforms it to C, JITs it, and runs it.
 """
 
 import logging
+
 logging.basicConfig(level=20)
 
 import numpy as np
@@ -19,110 +20,114 @@ from ctree.types import get_ctree_type
 # Specializer code
 
 class OpTranslator(LazySpecializedFunction):
-  def args_to_subconfig(self, args):
-    """
-    Analyze arguments and return a 'subconfig', a hashable object
-    that classifies them. Arguments with identical subconfigs
-    might be processed by the same generated code.
-    """
-    A = args[0]
-    return (len(A), A.dtype, A.ndim, A.shape)
+    def args_to_subconfig(self, args):
+        """
+        Analyze arguments and return a 'subconfig', a hashable object
+        that classifies them. Arguments with identical subconfigs
+        might be processed by the same generated code.
+        """
+        A = args[0]
+        return (len(A), A.dtype, A.ndim, A.shape)
 
-  def transform(self, py_ast, program_config):
-    """
-    Convert the Python AST to a C AST according to the directions
-    given in program_config.
-    """
-    len_A, A_dtype, A_ndim, A_shape = program_config[0]
-    inner_type = get_ctree_type(A_dtype)
-    array_type = NdPointer(A_dtype, A_ndim, A_shape)
-    apply_one_typesig = [inner_type, inner_type]
+    def transform(self, py_ast, program_config):
+        """
+        Convert the Python AST to a C AST according to the directions
+        given in program_config.
+        """
+        len_A, A_dtype, A_ndim, A_shape = program_config[0]
+        inner_type = get_ctree_type(A_dtype)
+        array_type = NdPointer(A_dtype, A_ndim, A_shape)
+        apply_one_typesig = [inner_type, inner_type]
 
-    tree = CFile("generated", [
-      py_ast.body[0],
-      FunctionDecl(Void(), "apply_all",
-        params=[SymbolRef("A", array_type)],
-        defn=[
-          For(Assign(SymbolRef("i", Int()), Constant(0)),
-              Lt(SymbolRef("i"), Constant(len_A)),
-              PostInc(SymbolRef("i")),
-              [
-                Assign(ArrayRef(SymbolRef("A"),SymbolRef("i")),
-                       FunctionCall(SymbolRef("apply"), [ArrayRef(SymbolRef("A"),
-                                                                  SymbolRef("i"))])),
-              ]),
-        ]
-      ),
-    ])
+        tree = CFile("generated", [
+            py_ast.body[0],
+            FunctionDecl(Void(), "apply_all",
+                         params=[SymbolRef("A", array_type)],
+                         defn=[
+                             For(Assign(SymbolRef("i", Int()), Constant(0)),
+                                 Lt(SymbolRef("i"), Constant(len_A)),
+                                 PostInc(SymbolRef("i")),
+                                 [
+                                     Assign(ArrayRef(SymbolRef("A"), SymbolRef("i")),
+                                            FunctionCall(SymbolRef("apply"), [ArrayRef(SymbolRef("A"),
+                                                                                       SymbolRef("i"))])),
+                                 ]),
+                         ]
+            ),
+        ])
 
-    tree = PyBasicConversions().visit(tree)
+        tree = PyBasicConversions().visit(tree)
 
-    apply_one = tree.find(FunctionDecl, name="apply")
-    apply_one.set_static().set_inline()
-    apply_one.set_typesig(apply_one_typesig)
+        apply_one = tree.find(FunctionDecl, name="apply")
+        apply_one.set_static().set_inline()
+        apply_one.set_typesig(apply_one_typesig)
 
-    return Project([tree])
+        return Project([tree])
 
 
 class ArrayOp(object):
-  """
-  A class for managing independent operation on elements
-  in numpy arrays.
-  """
-  def __init__(self):
-    """Instantiate translator."""
-    self.c_apply_all = OpTranslator(get_ast(self.apply), "apply_all")
+    """
+    A class for managing independent operation on elements
+    in numpy arrays.
+    """
 
-  def __call__(self, A):
-    """Apply the operator to the arguments via a generated function."""
-    return self.c_apply_all(A)
+    def __init__(self):
+        """Instantiate translator."""
+        self.c_apply_all = OpTranslator(get_ast(self.apply), "apply_all")
+
+    def __call__(self, A):
+        """Apply the operator to the arguments via a generated function."""
+        return self.c_apply_all(A)
 
 
 # ---------------------------------------------------------------------------
 # User code
 
 class Doubler(ArrayOp):
-  """Double elements of the array."""
-  def apply(n):
-    return n*2
+    """Double elements of the array."""
+
+    def apply(n):
+        return n * 2
+
 
 def py_doubler(A):
-  for i in xrange(len(A)):
-    A[i] *= 2
+    for i in xrange(len(A)):
+        A[i] *= 2
+
 
 def main():
-  c_doubler = Doubler()
+    c_doubler = Doubler()
 
-  # doubling doubles
-  actual_d   = np.ones(12, dtype=np.float64)
-  expected_d = np.ones(12, dtype=np.float64)
-  c_doubler(actual_d)
-  py_doubler(expected_d)
-  np.testing.assert_array_equal(actual_d, expected_d)
+    # doubling doubles
+    actual_d = np.ones(12, dtype=np.float64)
+    expected_d = np.ones(12, dtype=np.float64)
+    c_doubler(actual_d)
+    py_doubler(expected_d)
+    np.testing.assert_array_equal(actual_d, expected_d)
 
-  # doubling floats
-  actual_f   = np.ones(13, dtype=np.float32)
-  expected_f = np.ones(13, dtype=np.float32)
-  c_doubler(actual_f)
-  py_doubler(expected_f)
-  np.testing.assert_array_equal(actual_f, expected_f)
+    # doubling floats
+    actual_f = np.ones(13, dtype=np.float32)
+    expected_f = np.ones(13, dtype=np.float32)
+    c_doubler(actual_f)
+    py_doubler(expected_f)
+    np.testing.assert_array_equal(actual_f, expected_f)
 
-  # doubling ints
-  actual_i   = np.ones(14, dtype=np.int32)
-  expected_i = np.ones(14, dtype=np.int32)
-  c_doubler(actual_i)
-  py_doubler(expected_i)
-  np.testing.assert_array_equal(actual_i, expected_i)
+    # doubling ints
+    actual_i = np.ones(14, dtype=np.int32)
+    expected_i = np.ones(14, dtype=np.int32)
+    c_doubler(actual_i)
+    py_doubler(expected_i)
+    np.testing.assert_array_equal(actual_i, expected_i)
 
-  # demonstrate caching
-  c_doubler(actual_i)
-  c_doubler(actual_i)
-  py_doubler(expected_i)
-  py_doubler(expected_i)
-  np.testing.assert_array_equal(actual_i, expected_i)
+    # demonstrate caching
+    c_doubler(actual_i)
+    c_doubler(actual_i)
+    py_doubler(expected_i)
+    py_doubler(expected_i)
+    np.testing.assert_array_equal(actual_i, expected_i)
 
-  print "Success."
+    print "Success."
 
 
 if __name__ == '__main__':
-  main()
+    main()
--- examples/OclDoubler.py
+++ examples/OclDoubler.py
@@ -3,6 +3,7 @@ Parses the python AST below, transforms it to C, JITs it, and runs it.
 """
 
 import logging
+
 logging.basicConfig(level=20)
 
 import os
@@ -22,100 +23,106 @@ from ctree.types import pytype_to_ctype, get_ctype
 # Specializer code
 
 class OpTranslator(LazySpecializedFunction):
-  def args_to_subconfig(self, args):
-    """
-    Analyze arguments and return a 'subconfig', a hashable object
-    that classifies them. Arguments with identical subconfigs
-    might be processed by the same generated code.
-    """
-    A = args[0]
-    return (len(A), A.dtype, A.ndim, A.shape)
+    def args_to_subconfig(self, args):
+        """
+        Analyze arguments and return a 'subconfig', a hashable object
+        that classifies them. Arguments with identical subconfigs
+        might be processed by the same generated code.
+        """
+        A = args[0]
+        return (len(A), A.dtype, A.ndim, A.shape)
 
-  def transform(self, py_ast, program_config):
-    """
-    Convert the Python AST to a C AST according to the directions
-    given in program_config.
-    """
-    len_A, A_dtype, A_ndim, A_shape = program_config[0]
-    inner_type = pytype_to_ctype(A_dtype)
+    def transform(self, py_ast, program_config):
+        """
+        Convert the Python AST to a C AST according to the directions
+        given in program_config.
+        """
+        len_A, A_dtype, A_ndim, A_shape = program_config[0]
+        inner_type = pytype_to_ctype(A_dtype)
 
-    apply_one = PyBasicConversions().visit(py_ast.body[0])
-    apply_one_typesig = [inner_type, inner_type]
-    apply_one.set_typesig(apply_one_typesig)
+        apply_one = PyBasicConversions().visit(py_ast.body[0])
+        apply_one_typesig = [inner_type, inner_type]
+        apply_one.set_typesig(apply_one_typesig)
 
-    apply_all = FunctionDecl(None, "apply_all",
-      params=[SymbolRef("A", ct.POINTER(inner_type)), SymbolRef("n", ct.c_int)],
-      defn=[
-        Assign(SymbolRef("i", ct.c_int), FunctionCall(SymbolRef("get_global_id"), [Constant(0)])),
-        If(Lt(SymbolRef("i"), SymbolRef("n")), [
-          Assign(ArrayRef(SymbolRef("A"), SymbolRef("i")),
-                 FunctionCall(SymbolRef("apply"), [ArrayRef(SymbolRef("A"), SymbolRef("i"))]))
+        apply_all = FunctionDecl(None, "apply_all",
+                                 params=[SymbolRef("A", ct.POINTER(inner_type)), SymbolRef("n", ct.c_int)],
+                                 defn=[
+                                     Assign(SymbolRef("i", ct.c_int),
+                                            FunctionCall(SymbolRef("get_global_id"), [Constant(0)])),
+                                     If(Lt(SymbolRef("i"), SymbolRef("n")), [
+                                         Assign(ArrayRef(SymbolRef("A"), SymbolRef("i")),
+                                                FunctionCall(SymbolRef("apply"),
+                                                             [ArrayRef(SymbolRef("A"), SymbolRef("i"))]))
+                                     ])
+                                 ])
+        apply_all.set_kernel()
+
+        kernel = OclFile("kernel", [apply_one, apply_all])
+        kernel_path_ref = kernel.get_generated_path_ref()
+
+        control = CFile("control", [
+            CppInclude("OpenCL/opencl.h"),
+            Assign(SymbolRef("kernel_path", ct.c_char_p), kernel_path_ref),
+            FunctionDecl(None, "apply_all",
+                         params=[SymbolRef("A")],
+                         defn=[Return()]
+            ),
         ])
-      ])
-    apply_all.set_kernel()
 
-    kernel = OclFile("kernel", [apply_one, apply_all])
-    kernel_path_ref = kernel.get_generated_path_ref()
+        tree = Project([kernel, control])
 
-    control = CFile("control", [
-      CppInclude("OpenCL/opencl.h"),
-      Assign(SymbolRef("kernel_path", ct.c_char_p), kernel_path_ref),
-      FunctionDecl(None, "apply_all",
-        params=[SymbolRef("A")],
-        defn=[ Return() ]
-      ),
-    ])
+        transformations = [
+            ConvertNumpyNdpointers(),
+            StripPythonDocstrings(),
+        ]
+        for xf in transformations:
+            tree = xf.visit(tree)
 
-    tree = Project([kernel, control])
-
-    transformations = [
-      ConvertNumpyNdpointers(),
-      StripPythonDocstrings(),
-    ]
-    for xf in transformations:
-      tree = xf.visit(tree)
-
-    return tree
+        return tree
 
 
 class ArrayOp(object):
-  """
-  A class for managing independent operation on elements
-  in numpy arrays.
-  """
-  def __init__(self):
-    """Instantiate translator."""
-    self.c_apply_all = OpTranslator(get_ast(self.apply), "apply_all")
+    """
+    A class for managing independent operation on elements
+    in numpy arrays.
+    """
 
-  def __call__(self, A):
-    """Apply the operator to the arguments via a generated function."""
-    return self.c_apply_all(A)
+    def __init__(self):
+        """Instantiate translator."""
+        self.c_apply_all = OpTranslator(get_ast(self.apply), "apply_all")
+
+    def __call__(self, A):
+        """Apply the operator to the arguments via a generated function."""
+        return self.c_apply_all(A)
 
 
 # ---------------------------------------------------------------------------
 # User code
 
 class Doubler(ArrayOp):
-  """Double elements of the array."""
-  def apply(x):
-    return x*2
+    """Double elements of the array."""
+
+    def apply(x):
+        return x * 2
+
 
 def py_doubler(A):
-  for i in xrange(len(A)):
-    A[i] *= 2
+    for i in xrange(len(A)):
+        A[i] *= 2
+
 
 def main():
-  c_doubler = Doubler()
+    c_doubler = Doubler()
 
-  # doubling doubles
-  actual_d   = np.ones(12, dtype=np.float64)
-  expected_d = np.ones(12, dtype=np.float64)
-  c_doubler(actual_d)
-  py_doubler(expected_d)
-  np.testing.assert_array_equal(actual_d, expected_d)
+    # doubling doubles
+    actual_d = np.ones(12, dtype=np.float64)
+    expected_d = np.ones(12, dtype=np.float64)
+    c_doubler(actual_d)
+    py_doubler(expected_d)
+    np.testing.assert_array_equal(actual_d, expected_d)
 
-  print "Success."
+    print "Success."
 
 
 if __name__ == '__main__':
-  main()
+    main()
