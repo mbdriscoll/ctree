e39744a0b04795bad459af087b06ca37610de83e
chick <chick@eecs.berkeley.edu>
2014-03-10 09:45:09 -0700

    problems found trying to use timeit with conway life

--- ctree/visual/dot_manager.py
+++ ctree/visual/dot_manager.py
@@ -1,25 +1,5 @@
 __author__ = 'Chick Markley'
 
-from textwrap import dedent
-
-import ctree
-
-def singleton(cls):
-    instance = cls()
-    instance.__call__ = lambda: instance
-    return instance
-
-def truncate(text):
-    max_display_lines = ctree.CONFIG.getint("log", "max_lines_per_source")
-    n_lines = len(text.splitlines())
-    if n_lines <= max_display_lines:
-        return text
-    else:
-        return dedent("""\
-        // %d lines suppressed, only %d allowed
-        // consider adjusting config entry log.max_lines_per_source""" \
-        % (n_lines, max_display_lines))
-
 class DotManager(object):
     """
     take ast and return an ipython image file
--- examples/stencil_grid/conway_life.py
+++ examples/stencil_grid/conway_life.py
@@ -41,11 +41,33 @@ class ConwayKernel(StencilKernel):
             out_img[x] = new_state_map[int(out_img[x])]
 
 
+class IteratedConwayKernel(StencilKernel):
+    """
+    in_img is the life board at time t
+    out_img is the life board at time t+1
+    new_state_map defines the output state for a cell
+        first 8 indices are for dead cell, next 8 are for live cell
+        value at index is the new cell state
+    """
+
+    def __init__(self, generations):
+        self.generations = generations
+
+    def kernel(self, in_img, new_state_map, out_img):
+        for generation in range(self.generations):
+            for x in out_img.interior_points():
+                out_img[x] = in_img[x] * 8
+                for y in in_img.neighbors(x, 2):
+                    out_img[x] += in_img[y]
+                out_img[x] = new_state_map[int(out_img[x])]
+
+
 def run_game(width=25, height=25, generations=1):
     """play the game on board of specified size"""
 
     kernel = ConwayKernel()
     kernel.should_unroll = False
+    # kernel.pure_python = True
 
     # create a stencil grid for t+1
     current_grid = StencilGrid([height, width])
@@ -63,20 +85,39 @@ def run_game(width=25, height=25, generations=1):
     for index, new_state in enumerate([0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0]):
         new_state_map[index] = new_state
 
-    # render(current_grid, "Original input")
+    #render(current_grid, "Original input")
 
     for generation in range(generations):
         kernel.kernel(current_grid, new_state_map, future_grid)
         current_grid, future_grid = future_grid, current_grid
+        print("gen %s" % generation)
 
-    render(current_grid, "\nStencil version of kernel gives")
+    # render(current_grid, "\nStencil version of kernel gives")
+
+class GameRunner(object):
+    def __init__(self, width, height, generations):
+        self.width = width
+        self.height = height
+        self.generations = generations
+
+    def __call__(self, *args, **kwargs):
+        run_game(self.width, self.height, self.generations)
+
+    def run(self):
+        run_game(self.width, self.height, self.generations)
+
 
 if __name__ == '__main__':
     import sys
+    import timeit
     parameters = len(sys.argv)
 
     width = 25 if parameters < 2 else int(sys.argv[1])
     height = 25 if parameters < 3 else int(sys.argv[2])
     generations = 1 if parameters < 4 else int(sys.argv[3])
-    run_game(width, height, generations)
+
+    game_runner = GameRunner(width, height, generations)
+
+    #game_runner()
+    timeit.timeit(stmt=game_runner, number=10)
 
