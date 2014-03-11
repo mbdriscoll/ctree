fb47ba4339bb24973c6c94bf9f9d4dc3d287b4f1
Michael Driscoll <mbdriscoll@gmail.com>
2014-03-10 17:32:09 -0700

    run one OpenTuner test

--- ctree/opentuner/driver.py
+++ ctree/opentuner/driver.py
@@ -5,58 +5,14 @@ import threading
 import argparse
 import Queue as queue # queue in 3.x
 
-import ctree
+from ctree import CONFIG
 from ctree.tune import TuningDriver
 
 import opentuner
 from opentuner.measurement import MeasurementInterface
 from opentuner.resultsdb.models import Result
 from opentuner.tuningrunmain import TuningRunMain
-import opentuner.search.manipulator as ot
-
-# copy relevant names into ctree.tune namespace
-ConfigurationManipulator = ot.ConfigurationManipulator
-Parameter              = ot.Parameter
-PrimitiveParameter     = ot.PrimitiveParameter
-NumericParameter       = ot.NumericParameter
-IntegerParameter       = ot.IntegerParameter
-FloatParameter         = ot.FloatParameter
-ScaledNumericParameter = ot.ScaledNumericParameter
-LogIntegerParameter    = ot.LogIntegerParameter
-LogFloatParameter      = ot.LogFloatParameter
-PowerOfTwoParameter    = ot.PowerOfTwoParameter
-ComplexParameter       = ot.ComplexParameter
-BooleanParameter       = ot.BooleanParameter
-SwitchParameter        = ot.SwitchParameter
-EnumParameter          = ot.EnumParameter
-PermutationParameter   = ot.PermutationParameter
-ScheduleParameter      = ot.ScheduleParameter
-SelectorParameter      = ot.SelectorParameter
-ArrayParameter         = ot.ArrayParameter
-BooleanArrayParameter  = ot.BooleanArrayParameter
-ParameterProxy         = ot.ParameterProxy
-
-class OpenTunerThread(threading.Thread):
-    """
-    Thread to drive OpenTuner.
-    """
-    def __init__(self, manipulator, results_queue, configs_queue):
-        super(OpenTunerThread, self).__init__()
-        self._manipulator = manipulator
-        self._results = results_queue
-        self._configs = configs_queue
-        self.daemon = True
-
-    def run(self):
-        """Starts the main OpenTuner loop."""
-        from ctree import CONFIG
-
-        log.info("tuning thread '%s' starting.", self.name)
-        arg_parser = argparse.ArgumentParser(parents=opentuner.argparsers())
-        tuner_args = arg_parser.parse_args(CONFIG.get("tuning", "opentuner_args").split())
-        interface = CtreeMeasurementInterface(self._manipulator, self._results, self._configs)
-        TuningRunMain(interface, tuner_args).main()
-        log.info("tuning thread '%s' terminating.", self.name)
+from opentuner.search.manipulator import ConfigurationManipulator
 
 
 class OpenTunerDriver(TuningDriver):
@@ -65,36 +21,66 @@ class OpenTunerDriver(TuningDriver):
     a stream of configurations, as well as an interface
     to report on the performance of each.
     """
-    def __init__(self, manipulator):
+    def __init__(self, manipulator, opentuner_args=None):
         """
         Creates communication queues and spawn a thread
         to run the tuning logic.
         """
+        assert isinstance(manipulator, ConfigurationManipulator)
         self._results = queue.Queue(1)
         self._configs = queue.Queue(1)
-        OpenTunerThread(manipulator, self._results, self._configs).start()
+        ot_args = opentuner_args if opentuner_args else []
+        self._thread = OpenTunerThread(manipulator, self._results, self._configs, ot_args)
+        self._thread.start()
 
     def get_next_config(self):
         """Get the next configuration to test."""
-        from ctree import CONFIG
-
-        timeout = CONFIG.getint("tuning", "timeout")
+        timeout = CONFIG.getint("opentuner", "timeout")
         try:
             return self._configs.get(True, timeout)
         except queue.Empty:
             log.warning("exhausted stream of tuning configurations")
         return None
 
-    def report(self, time=float('inf'), accuracy=None, energy=None, size=None, confidence=None):
+    def report(self, **kwargs):
         """Report the performance of the most recent configuration."""
-        from ctree import CONFIG
-
-        timeout = CONFIG.getint("tuning", "timeout")
-        result = Result(time=time, accuracy=accuracy, energy=energy, size=size, confidence=confidence)
+        timeout = CONFIG.getint("opentuner", "timeout")
+        result = Result(**kwargs)
         try:
             self._results.put(result, True, timeout)
         except queue.Full:
             log.warning("exhausted stream of tuning configurations")
+
+
+class OpenTunerThread(threading.Thread):
+    """
+    Thread to drive OpenTuner.
+    """
+    def __init__(self, manipulator, results_queue, configs_queue, opentuner_args=None):
+        super(OpenTunerThread, self).__init__()
+        self._manipulator = manipulator
+        self._results = results_queue
+        self._configs = configs_queue
+        self._opentuner_args = opentuner_args if opentuner_args else []
+        assert isinstance(opentuner_args, list), \
+            "Expected a list of OpenTuner args, but got an '%s'." % \
+            type(opentuner_args)
+        self._tuningrun = None
+
+        # variables for Thread class
+        self.name = "opentuner_driver"
+        self.daemon = True
+
+    def run(self):
+        """Starts the main OpenTuner loop."""
+        log.info("tuning thread '%s' starting (%d total threads now).", \
+            self.name, threading.active_count())
+        arg_parser = argparse.ArgumentParser(parents=opentuner.argparsers())
+        config_args = CONFIG.get("opentuner", "args").split()
+        tuner_args = arg_parser.parse_args(config_args + self._opentuner_args)
+        interface = CtreeMeasurementInterface(self._manipulator, self._results, self._configs)
+        TuningRunMain(interface, tuner_args).main()
+        log.info("tuning thread '%s' terminating.", self.name)
 
 
 class CtreeMeasurementInterface(MeasurementInterface):
@@ -124,9 +110,7 @@ class CtreeMeasurementInterface(MeasurementInterface):
         its performance. With threading, we place the config in the
         output queue, then wait for a Result in the input queue.
         """
-        from ctree import CONFIG
-
-        timeout = CONFIG.getint("tuning", "timeout")
+        timeout = CONFIG.getint("opentuner", "timeout")
         self._configs.put(desired_result.configuration, True, timeout)
         return self._results.get(True, timeout)
 
--- test/test_tuning.py
+++ test/test_tuning.py
@@ -1,14 +1,56 @@
 import unittest
 
+import os
+import shutil
 
-class TestOpenTuner(unittest.TestCase):
+class TestNullTuningDriver(unittest.TestCase):
     def test_import(self):
         import ctree.tune
 
-    def test_import_opentuner(self):
-        import ctree.opentuner
+    def test_null_driver_stream(self):
+        from ctree.tune import NullTuningDriver
 
-    def test_make_int_param(self):
-        from ctree.opentuner.driver import IntegerParameter
+        driver = NullTuningDriver()
+        for i in range(4):
+            self.assertIsNone(driver.get_next_config())
 
-        IntegerParameter("block_size", 1, 10)
+    def test_null_driver_report(self):
+        from ctree.tune import NullTuningDriver
+
+        driver = NullTuningDriver()
+        for i in range(4):
+            config = driver.get_next_config()
+            driver.report(time=0.4)
+
+
+try:
+    import opentuner
+except ImportError:
+    HAVE_OPENTUNER = False
+else:
+    HAVE_OPENTUNER = True
+
+class TestOpenTunerDriver(unittest.TestCase):
+    @unittest.skipUnless(HAVE_OPENTUNER, "OpenTuner not in PYTHONPATH.")
+    def test_import(self):
+        from ctree.opentuner.driver import OpenTunerDriver
+
+    @unittest.skipUnless(HAVE_OPENTUNER, "OpenTuner not in PYTHONPATH.")
+    def test_1d_config(self):
+        from ctree.opentuner.driver import OpenTunerDriver
+        from opentuner.search.manipulator import (
+            ConfigurationManipulator,
+            IntegerParameter,
+        )
+
+        cfg = ConfigurationManipulator()
+        cfg.add_parameter( IntegerParameter("foo", 0, 199) )
+        driver = OpenTunerDriver(cfg)
+
+        unsearched = set(range(200))
+        for i in range(20):
+            cfg = driver.get_next_config()
+            val = cfg.data["foo"]
+            driver.report(time=val)
+            unsearched.remove(val)
+        self.assertEqual(len(unsearched), 180)
