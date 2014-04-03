bf97e72cc1e43df2ed7775e7eef1f7e3cf1f6111
chick <chick@eecs.berkeley.edu>
2014-04-03 16:57:17 -0700

    add test module

---
+++ test/test_dot_manager.py
@@ -0,0 +1,22 @@
+__author__ = 'Chick Markley'
+
+import unittest
+
+from ctree.visual.dot_manager import DotManager
+from inspect import getsource
+from ctree.frontend import get_ast
+from fixtures.sample_asts import *
+
+
+class TestDotManager(unittest.TestCase):
+    """
+    Difficult to test because of ipython and dot dependencies
+    """
+
+    def test_c_identity(self):
+        tree = get_ast(getsource(square_of))
+        DotManager.run_dot(tree)
+
+
+def square_of(n):
+    return n * n
