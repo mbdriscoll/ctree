c8c6ebaa3401ab18ab1c0172a36968b53bf2eac5
Leonard Truong <leonardtruong@gmail.com>
2014-08-12 19:50:23 -0700

    Formatting.

--- test/fixtures/sample_asts.py
+++ test/fixtures/sample_asts.py
@@ -43,17 +43,20 @@ def gcd(a, b):
 
 
 gcd_ast = \
-    FunctionDecl(c_int(), "gcd", [SymbolRef("a", c_int()), SymbolRef("b", c_int())], [
-        If(Eq(SymbolRef('b'), Constant(0)),
-           [Return(SymbolRef('a'))],
-           [Return(FunctionCall(SymbolRef('gcd'), [SymbolRef('b'), Mod(SymbolRef('a'),
-                                                                       SymbolRef('b'))]))])
-    ])
+    FunctionDecl(c_int(), "gcd",
+                 [SymbolRef("a", c_int()), SymbolRef("b", c_int())], [
+                     If(Eq(SymbolRef('b'), Constant(0)),
+                        [Return(SymbolRef('a'))],
+                        [Return(FunctionCall(SymbolRef('gcd'),
+                                [SymbolRef('b'), Mod(SymbolRef('a'),
+                                                     SymbolRef('b'))]))])
+                 ])
 
 SAMPLE_ASTS.append((gcd, gcd_ast))
 
 # ---------------------------------------------------------------------------
 # naive fibonacci
+
 
 def fib(n):
     if n < 2:
@@ -66,14 +69,17 @@ fib_ast = \
     FunctionDecl(c_int(), "fib", [SymbolRef("n", c_int())], [
         If(Lt(SymbolRef("n"), Constant(2)),
            [Return(SymbolRef("n"))],
-           [Return(Add(FunctionCall(SymbolRef("fib"), [Sub(SymbolRef("n"), Constant(1))]),
-                       FunctionCall(SymbolRef("fib"), [Sub(SymbolRef("n"), Constant(2))])))])
+           [Return(Add(FunctionCall(SymbolRef("fib"),
+                                    [Sub(SymbolRef("n"), Constant(1))]),
+                       FunctionCall(SymbolRef("fib"),
+                                    [Sub(SymbolRef("n"), Constant(2))])))])
     ])
 
 SAMPLE_ASTS.append((fib, fib_ast))
 
 # ---------------------------------------------------------------------------
 # a zero-argument function
+
 
 def get_two():
     return 2
@@ -89,6 +95,7 @@ SAMPLE_ASTS.append((get_two, get_two_ast))
 # ---------------------------------------------------------------------------
 # a function with mixed argument types
 
+
 def choose(p, a, b):
     if p < 0.5:
         return a
@@ -98,13 +105,14 @@ def choose(p, a, b):
 
 choose_ast = \
     FunctionDecl(c_long(), "choose",
-                 [SymbolRef("p", c_double()), SymbolRef("a", c_long()), SymbolRef("b", c_long())], [
-            If(Lt(SymbolRef("p"), Constant(0.5)), [
-                Return(SymbolRef("a")),
-            ], [
-                   Return(SymbolRef("b")),
-               ])
-        ])
+                 [SymbolRef("p", c_double()),
+                  SymbolRef("a", c_long()),
+                  SymbolRef("b", c_long())],
+                 [
+                     If(Lt(SymbolRef("p"), Constant(0.5)),
+                        [Return(SymbolRef("a"))],
+                        [Return(SymbolRef("b"))])
+                 ])
 
 SAMPLE_ASTS.append((choose, choose_ast))
 
@@ -114,27 +122,33 @@ SAMPLE_ASTS.append((choose, choose_ast))
 import math
 import numpy as np
 
+
 def l2norm(A):
     return math.sqrt(sum(x*x for x in A))
 
 l2norm_ast = CFile("generated", [
     CppInclude("math.h"),
     FunctionDecl(c_double(), "l2norm",
-        params=[
-            SymbolRef("A", np.ctypeslib.ndpointer(dtype=np.float64, ndim=1, shape=(12,))()),
-            SymbolRef("n", c_int()),
-        ],
-        defn=[
-            SymbolRef("sum", c_double()),
-            For(Assign(SymbolRef("i", c_int()), Constant(0)),
-                Lt(SymbolRef("i"), SymbolRef("n")),
-                PostInc(SymbolRef("i")), [
-                AddAssign(SymbolRef("sum"),
-                          Mul(ArrayRef(SymbolRef("A"), SymbolRef("i")),
-                              ArrayRef(SymbolRef("A"), SymbolRef("i")))),
-        ]),
-        Return( FunctionCall("sqrt", [SymbolRef("sum")]) ),
-    ])
+                 params=[
+                     SymbolRef("A",
+                               np.ctypeslib.ndpointer(
+                                   dtype=np.float64, ndim=1, shape=(12,)
+                               )()),
+                     SymbolRef("n", c_int()),
+                     ],
+                 defn=[
+                     SymbolRef("sum", c_double()),
+                     For(Assign(SymbolRef("i", c_int()), Constant(0)),
+                         Lt(SymbolRef("i"), SymbolRef("n")),
+                         PostInc(SymbolRef("i")), [
+                             AddAssign(SymbolRef("sum"),
+                                       Mul(ArrayRef(SymbolRef("A"),
+                                                    SymbolRef("i")),
+                                           ArrayRef(SymbolRef("A"),
+                                                    SymbolRef("i")))),
+                         ]),
+                     Return(FunctionCall("sqrt", [SymbolRef("sum")])),
+                 ])
 ])
 
 SAMPLE_ASTS.append((l2norm, l2norm_ast))
