e195ff9a074d4fb4d0ea2407ecdfea21ce4c6683
Leonard Truong <leonardtruong@gmail.com>
2014-08-15 11:29:55 -0700

    Bumping version for release

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ data_file_list = make_data_file_list("ctree/tools/generators/templates", "ctree/
 
 setup(
     name='ctree',
-    version='0.95a3',
+    version='0.95a4',
     description='A C-family AST implementation designed to be an IR for DSL compilers.',
 
     packages=[
