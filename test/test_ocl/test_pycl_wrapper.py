3d1b3a236e1d50f7af686a85010d1c0b7aaa02be
Leonard Truong <leonardtruong@gmail.com>
2014-08-22 00:08:48 -0700

    Cache contexts and queues

--- test/test_ocl/test_pycl_wrapper.py
+++ test/test_ocl/test_pycl_wrapper.py
@@ -2,12 +2,13 @@ import unittest
 
 import pycl as cl
 
-from ctree.ocl import get_context_from_device
+from ctree.ocl import get_context_and_queue_from_devices
+
 
 class TestCacheContexts(unittest.TestCase):
-	def test_simple_cache(self):
-		devices = cl.clGetDeviceIDs()
-		device = devices[-1]
-		ctx1 = get_context_from_device(device)
-		ctx2 = get_context_from_device(device)
-		self.assertEqual(ctx1, ctx2)
+    def test_simple_cache(self):
+        devices = cl.clGetDeviceIDs()
+        device = devices[-1]
+        results1 = get_context_and_queue_from_devices([device])
+        results2 = get_context_and_queue_from_devices([device])
+        self.assertEqual(results1, results2)
