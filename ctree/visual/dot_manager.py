0bd8e3960262886accf766d38f8eec4231caaee9
Nathan Zhang <nzhang-dev@airbears2-10-142-13-197.airbears2.1918.berkeley.edu>
2014-10-03 10:18:54 -0700

    Reorganized imports, made run_dot in charge of file operations since it already takes a file_name argument and the others seem to take run_dot's output and write it to file_name anyway.

--- ctree/visual/dot_manager.py
+++ ctree/visual/dot_manager.py
@@ -1,5 +1,12 @@
 __author__ = 'Chick Markley'
 
+
+import os
+from subprocess import Popen, PIPE, check_output
+from sphinx.util.osutil import EPIPE, EINVAL
+
+import warnings
+
 class DotManager(object):
     """
     take ast and return an ipython image file
@@ -13,21 +20,15 @@ class DotManager(object):
     @staticmethod
     def dot_ast_to_browser(ast_node, file_name):
         dot_text = ast_node.to_dot()
-        dot_output = DotManager.run_dot(dot_text)
+        dot_output = DotManager.run_dot(dot_text, file_name=file_name)
 
-        with open(file_name, "wb") as f:
-            f.write(dot_output)
-
-        import subprocess
         subprocess.check_output(["open", file_name])
 
     @staticmethod
     def dot_ast_to_file(ast_node, file_name):
         dot_text = ast_node.to_dot()
-        dot_output = DotManager.run_dot(dot_text)
+        dot_output = DotManager.run_dot(dot_text, filename)
 
-        with open(file_name, "wb") as f:
-            f.write(dot_output)
 
     @staticmethod
     def dot_text_to_image(text):
@@ -36,26 +37,27 @@ class DotManager(object):
 
             dot_output = DotManager.run_dot(text)
             return Image(dot_output, embed=True)
-        except:
+        except Exception as e:
+            warnings.warn('An error occured while attempting to create Image.')
             return None
 
     @staticmethod
     def run_dot(code, options=None, output_format='png', file_name=None):
         # mostly copied from sphinx.ext.graphviz.render_dot
-        import os
-        from subprocess import Popen, PIPE
-        from sphinx.util.osutil import EPIPE, EINVAL
 
         if not options:
             options = []
         dot_args = ['dot'] + options + ['-T', output_format]
         if file_name:
-            dot_args += ['>',file_name]
+            dot_args += ['-o',file_name]
+
 
         if os.name == 'nt':
             # Avoid opening shell window.
             # * https://github.com/tkf/ipython-hierarchymagic/issues/1
             # * http://stackoverflow.com/a/2935727/727827
+            # * http://msdn.microsoft.com/en-us/library/ms684863%28v=VS.85%29.aspx
+            # 0x08000000 is the CREATE_NO_WINDOW Process Creation Flag on Windows XP+
             p = Popen(dot_args, stdout=PIPE, stdin=PIPE, stderr=PIPE,
                       creationflags=0x08000000)
         else:
