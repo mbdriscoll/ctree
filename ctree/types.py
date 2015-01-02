568fb881d21ead9c6d7ea3bc0e058e3f679b8ae5
Nathan Zhang <nzhang32@berkeley.edu>
2015-01-01 21:14:19 -0800

    quick patch to get_common_ctypes for non numeric types

--- ctree/types.py
+++ ctree/types.py
@@ -120,5 +120,7 @@ def get_common_ctype(ctypes_list):
     #lowest ranking takes precedence
     rankings = [ctypes.c_longdouble, ctypes.c_double, ctypes.c_float, ctypes.uint, ctypes.int, ctypes.c_byte,
                 ctypes.c_wchar, ctypes.c_char, ctypes.c_bool, None]
-
-    return min(ctypes_list, key=rankings.index)
+    try:
+        return min(ctypes_list, key=rankings.index)
+    except ValueError:
+        return ctypes_list[0]
