bff841b7e64cc633ba12f4af76e272bc75ab7263
Mihir Patil <mihir.patil@berkeley.edu>
2015-02-09 22:07:26 -0800

    Added cache enable/disable command to ctree.

--- ctree/tools/runner.py
+++ ctree/tools/runner.py
@@ -5,6 +5,7 @@ basically copies all files and directories from a template.
 
 import sys
 import argparse
+import ctree
 from ctree.tools.generators import builder as Builder
 
 
@@ -24,6 +25,8 @@ def main(*args):
     )
     parser.add_argument('-p', '--port', help="/dev name to use for wattsup meter port")
     parser.add_argument('-v', '--verbose', help='show more debug than you like', action="store_true")
+    parser.add_argument('-dc', '--disable_caching', help='disable the persistent caching mechanism', action="store_true")
+    parser.add_argument('-ec', '--enable_caching', help='enable the persistent caching mechanism', action="store_true")
     args = parser.parse_args(args)
 
     if args.startproject:
@@ -32,14 +35,29 @@ def main(*args):
         print "create project specializer %s" % specializer_name
 
         builder = Builder.Builder("create", specializer_name, verbose=args.verbose)
-
         builder.build(None, None)
+        
     elif args.wattsupmeter:
         from ctree.metrics.watts_up_reader import WattsUpReader
 
         port = args.port if args.port else WattsUpReader.guess_port()
         meter = WattsUpReader(port_name=port)
         meter.interactive_mode()
+
+    elif args.enable_caching:
+        ctree.CONFIG.set("jit", "CACHE_ON", value="True")
+
+        with open(ctree.CFG_PATHS[-1], 'w') as configfile:
+            ctree.CONFIG.write(configfile)
+            configfile.close()
+
+    elif args.disable_caching:
+        ctree.CONFIG.set("jit", "CACHE_ON", value="False")
+
+        with open(ctree.CFG_PATHS[-1], 'w') as configfile:
+            ctree.CONFIG.write(configfile)
+            configfile.close()
+
     else:
         parser.print_usage()
 
