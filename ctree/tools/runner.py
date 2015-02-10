28b3a48748d64666ca733cfad026a1933d5cc407
Mihir Patil <mihir.patil@berkeley.edu>
2015-02-09 22:43:04 -0800

    Added cache clearing mechanism as a ctree command.

--- ctree/tools/runner.py
+++ ctree/tools/runner.py
@@ -6,14 +6,17 @@ basically copies all files and directories from a template.
 import sys
 import argparse
 import ctree
+
 from ctree.tools.generators import builder as Builder
+from subprocess import call as shell
+
 
 
 __author__ = 'chick'
 
 
 def main(*args):
-    """run ctree utility stuff, currently only the project generator"""
+    '''run ctree utility stuff, currently only the project generator'''
 
     if sys.argv:
         args = sys.argv[1:]
@@ -25,8 +28,9 @@ def main(*args):
     )
     parser.add_argument('-p', '--port', help="/dev name to use for wattsup meter port")
     parser.add_argument('-v', '--verbose', help='show more debug than you like', action="store_true")
-    parser.add_argument('-dc', '--disable_caching', help='disable the persistent caching mechanism', action="store_true")
-    parser.add_argument('-ec', '--enable_caching', help='enable the persistent caching mechanism', action="store_true")
+    parser.add_argument('-dc', '--disable_caching', help='disable and delete the persistent cache', action="store_true")
+    parser.add_argument('-ec', '--enable_caching', help='enable the persistent cache', action="store_true")
+    parser.add_argument('-cc', '--clear_cache', help='clear the persistent cache', action="store_true")
     args = parser.parse_args(args)
 
     if args.startproject:
@@ -36,7 +40,7 @@ def main(*args):
 
         builder = Builder.Builder("create", specializer_name, verbose=args.verbose)
         builder.build(None, None)
-        
+
     elif args.wattsupmeter:
         from ctree.metrics.watts_up_reader import WattsUpReader
 
@@ -46,20 +50,46 @@ def main(*args):
 
     elif args.enable_caching:
         ctree.CONFIG.set("jit", "CACHE_ON", value="True")
-
-        with open(ctree.CFG_PATHS[-1], 'w') as configfile:
-            ctree.CONFIG.write(configfile)
-            configfile.close()
+        write_success = write_to_config()
+        if write_success: print("[SUCCESS] ctree caching enabled.")
 
     elif args.disable_caching:
         ctree.CONFIG.set("jit", "CACHE_ON", value="False")
+        write_success = write_to_config()
+        clear_cache()
+        if write_success: print("[SUCCESS] ctree caching disabled.")
 
-        with open(ctree.CFG_PATHS[-1], 'w') as configfile:
-            ctree.CONFIG.write(configfile)
-            configfile.close()
+    elif args.clear_cache:
+        clear_cache()
 
     else:
         parser.print_usage()
+
+
+def write_to_config():
+    '''
+    This method handles writing to the closest config file to the current
+    project, but does not write to the defaults.cfg file in ctree.
+    :return: return True if write is successful. False otherwise.
+    '''
+
+    if len(ctree.CFG_PATHS) > 0:
+        with open(ctree.CFG_PATHS[-1], 'w') as configfile:
+            ctree.CONFIG.write(configfile)
+            configfile.close()
+        return True
+    else:
+        print("[FAILURE] No config file detected. Please create a '.ctree.cfg' file in your project directory.")
+        return False
+
+
+def clear_cache():
+    '''
+    This method handles clearing the closest cache to the current project.
+    '''
+    path = ctree.CONFIG.get("jit", "COMPILE_PATH")
+    shell(["rm", "-rf", path])
+    print("[SUCCESS] ctree cache deleted from path: " + path)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
