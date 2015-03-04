a884c074ea1f4e69d548b0050090bd9448ca0fe2
Nathan Zhang <nzhang32@berkeley.edu>
2015-03-03 16:12:18 -0800

    arraydef bodies should be arrays, not python lists

--- test/test_ArrayDefs.py
+++ test/test_ArrayDefs.py
@@ -1,7 +1,7 @@
 import ctypes as ct
 
 from util import CtreeTest
-from ctree.c.nodes import SymbolRef, Constant, Add, Mul, ArrayDef, Sub
+from ctree.c.nodes import SymbolRef, Constant, Add, Mul, ArrayDef, Sub, Array
 
 
 class TestArrayDefs(CtreeTest):
@@ -9,16 +9,16 @@ class TestArrayDefs(CtreeTest):
     def test_simple_array_def(self):
         self._check_code(ArrayDef(
             SymbolRef('hi', ct.c_int()), Constant(2),
-            [Constant(0), Constant(1)]
+            Array(body=[Constant(0), Constant(1)]),
         ), "int hi[2] = { 0, 1 }")
 
     def test_complex(self):
         node = ArrayDef(
             SymbolRef('myArray', ct.c_int()),
             Constant(2),
-            [
+            Array(body=[
                 Add(SymbolRef('b'), SymbolRef('c')),
                 Mul(Sub(Constant(99), SymbolRef('d')), Constant(200))
-            ]
+            ])
         )
         self._check_code(node, "int myArray[2] = { b + c, (99 - d) * 200 }")
