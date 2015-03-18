b5c5853dc41fb3e11e97724fbb37b476b2769129
Leonard Truong <leonardtruong@gmail.com>
2015-03-17 23:32:02 -0700

    Add numpy type recognizers

--- ctree/np/__init__.py
+++ ctree/np/__init__.py
@@ -1,4 +1,5 @@
 import numpy as np
+import ctypes as ct
 
 from ctree.types import (
     codegen_type,
@@ -13,7 +14,32 @@ def codegen_ndptr(ndptr):
     return prefix + "%s*" % codegen_type(ndptr._dtype_.type())
 
 register_type_recognizers({
-    np.ndarray: lambda obj: np.ctypeslib.as_ctypes(obj)
+    np.ndarray: lambda obj: np.ctypeslib.as_ctypes(obj),
+    np.bool8:            ct.c_bool,
+
+    # signed integer types
+    np.byte:             ct.c_char,
+    np.short:            ct.c_short,
+    np.intc:             ct.c_int,
+    np.longlong:         ct.c_longlong,
+
+    # technically not universally compatible
+    np.int8:             ct.c_char,
+    np.int16:            ct.c_short,
+    np.int32:            ct.c_int,
+    np.int64:            ct.c_long,
+
+    # unsigned integer types
+    np.ubyte:            ct.c_ubyte,
+    np.ushort:           ct.c_ushort,
+    np.uintc:            ct.c_uint,
+    np.ulonglong:        ct.c_ulonglong,
+
+    # floating point types
+    np.single:           ct.c_float,
+    np.float32:          ct.c_float,
+    np.double:           ct.c_double,
+    np.float64:          ct.c_double,
 })
 
 register_type_codegenerators({
